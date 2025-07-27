7e98f02867dd8192d73ed17406dd49c67eee308d
Prashant Wakchaure <prashant.wakchuare@ucdconnect.ie>
2025-07-27 16:16:03 +0100

    changing path variable to id

--- src/main/java/com/grocery/recipes/model/Ingredient.java
+++ src/main/java/com/grocery/recipes/model/Ingredient.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -24,9 +23,7 @@ public class Ingredient {
     @NotBlank(message = "Ingredient name is required")
     private String name;
 
-    @DecimalMin(value = "0.0", inclusive = false, message = "Quantity must be greater than zero")
-    private double quantity;
-
     @NotNull(message = "Unit is required")
-    private String unit;  // e.g., g, ml, pcs ...
+    // e.g., g, ml, pcs, ...
+    private String unit;
 }
